cebf3e6d76161ef0f63a1363671edf07462c5fd9
IvanShpotenko <Ivan.shpotenko@gmail.com>
2017-01-08 13:46:44 +0200

    Update timer.py

--- timer.py
+++ timer.py
@@ -5,9 +5,9 @@ import time
 def timer(func, precision: int=4):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        c1, p1 = time.clock(), time.perf_counter()
+        c1, p1 = time.process_time(), time.perf_counter()
         var = func(*args, **kwargs)
-        c2, p2 = time.clock() - c1, time.perf_counter() - p1
+        c2, p2 = time.process_time() - c1, time.perf_counter() - p1
         print(" Function '{}' ran in {:.{p}f} s. ({:.{p}f} s. CPU only)".format(func.__name__, p2, c2,
                                                                                 p=precision))
         return var
